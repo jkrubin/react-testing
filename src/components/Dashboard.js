44471315eec74be56d40c15ed0b84f76a6af557e
Josh Rubin <jkrubin42@gmail.com>
2019-03-09 22:49:33 -0800

    user panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,23 @@
 import React from "react"
+import { AuthContext } from "../Contexts/AuthContext"
 
-const Dashboard = () => <h2>User Dashboard </h2>
+class Dashboard extends React.Component{
+	constructor(props, context){
+		super()
+		this.state = {
+			auth: context.auth,
+			token: false
+		}
+	}
 
+	render(){
+		return(
+			<div>
+				<h1> Hello {this.state.auth.user.name} </h1>
+			</div>
+		)
+	}
+}
+
+Dashboard.contextType = AuthContext
 export default Dashboard
