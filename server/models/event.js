d84468819409101889b1a1e773b5fc0e119f3533
Josh Rubin <jkrubin42@gmail.com>
2019-03-10 17:40:34 -0700

    event model

--- server/models/event.js
+++ server/models/event.js
@@ -1,10 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Event = sequelize.define('Event', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    image: DataTypes.STRING,
+    location: DataTypes.STRING
   }, {});
   Event.associate = function(models) {
-    // associations can be defined here
+    Event.belongsTo(models.users)
   };
   return Event;
 };
