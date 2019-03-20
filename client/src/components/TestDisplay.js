2547bf07aaba9304d687b8c3f04df4d2dff268b5
Josh Rubin <jkrubin42@gmail.com>
2019-03-19 18:08:15 -0700

    UI updates with homepage transitions

--- client/src/components/TestDisplay.js
+++ client/src/components/TestDisplay.js
@@ -9,9 +9,16 @@ class TestDisplay extends React.Component{
 			userId: props.userId,
 			isLoading: false,
 			error: false,
+			displayProf: false
 		}
 		this.submitLike = this.submitLike.bind(this)
 		this.deleteLike = this.deleteLike.bind(this)
+		this.toggleProfile = this.toggleProfile.bind(this)
+	}
+	toggleProfile(){
+		this.setState((prevState) =>{
+			return {displayProf: !prevState.displayProf}
+		})
 	}
 	submitLike(){
 		this.setState({
@@ -92,7 +99,7 @@ class TestDisplay extends React.Component{
 					<div className="row event-row" >
 						<div className="event-image-container">
 							<div className="event-image">
-								<img className="card-img-top" src={this.state.image} alt=""/>
+									<img className="card-img-top" src={this.state.image} alt="" />
 							</div>
 						</div>
 						<div className="event-content-container">
@@ -102,55 +109,75 @@ class TestDisplay extends React.Component{
 										<h5 className="card-title"> {this.state.name} </h5>
 									</div>
 								</li>
-								<li className="event-desc">
-									<div className="list-icon-container">
-										<img
-											src={require('../assets/infoIcon.png')}
-											height='15'
-											width='15'
-											alt=""
-										/>
-										<div className="border-container"> </div>
-									</div>
-									<div className="event-desc-div list-content">
-										<h6 className="card-text">Description: </h6>
-										<p className="card-text"> {this.state.description} </p>
-									</div>
-								</li>
-								<li className="event-loc">
-									<div className="list-icon-container">
-										<img
-											src={require('../assets/locationIcon.png')}
-											height='15'
-											width='15'
-											alt=""
-										/>
-										<div className="border-container"> </div>
-									</div>
-									<div className="event-location-div list-content">
-										<h6 className="card-text">Location: </h6>
-										<p className="card-text"> 
-											At {this.state.location}
-										</p>
+								<li className="event-user">
+									<div className={"list-profile-container " + (this.state.displayProf ? "show-profile" : "")}>
+										<div className="list-profile">
+											<img src={`data:${this.state.users.mimeType};base64,${this.state.users.profilePicture}`} 
+												alt="" 
+												className="event-profile-pic"
+												onClick={this.toggleProfile}/>
+										</div>
+										<div className="event-user-container ">
+											<div className="event-user">
+												<h4 className="event-username"> {this.state.users.name} </h4>
+												<div className={this.state.displayProf ? "expand-prof" : "min-prof"}>
+													<p className="card-text event-bio"> {this.state.users.bio} </p>
+												</div>
+											</div>
+										</div>
 									</div>
 								</li>
-								<li className="event-datetime">
-									<div className="list-icon-container">
-										<img
-											src={require('../assets/dateIcon.png')}
-											height='15'
-											width='15'
-											alt=""
-										/>
-										<div className="border-container"> </div>
-									</div>
-									<div className="event-date-div list-content">
-										<h6 className="card-text">Date: </h6>
-										<p className="card-text">
-											{displayDate}
-										</p>
-									</div>
-								</li>
+								<div className={(this.state.displayProf ? "" : "")}>
+									<li className="event-desc ">
+										<div className="list-icon-container list-flush ">
+											<img
+												src={require('../assets/infoIcon.png')}
+												height='15'
+												width='15'
+												alt=""
+											/>
+											<div className="border-container"> </div>
+										</div>
+										<div className="event-desc-div list-content">
+											<h6 className="card-text">Description: </h6>
+											<p className={"card-text " + (this.state.displayProf ? "hide-event-info" : "")}> {this.state.description} </p>
+										</div>
+									</li>
+									<li className="event-loc ">
+										<div className="list-icon-container list-flush">
+											<img
+												src={require('../assets/locationIcon.png')}
+												height='15'
+												width='15'
+												alt=""
+											/>
+											<div className="border-container"> </div>
+										</div>
+										<div className="event-location-div list-content">
+											<h6 className="card-text">Location: </h6>
+											<p className={"card-text " + (this.state.displayProf ? "hide-event-info" : "")}> 
+												At {this.state.location}
+											</p>
+										</div>
+									</li>
+									<li className="event-datetime ">
+										<div className="list-icon-container list-flush">
+											<img
+												src={require('../assets/dateIcon.png')}
+												height='15'
+												width='15'
+												alt=""
+											/>
+											<div className="border-container"> </div>
+										</div>
+										<div className="event-date-div list-content">
+											<h6 className="card-text">Date: </h6>
+											<p className={"card-text " + (this.state.displayProf ? "hide-event-info" : "")}>
+												{displayDate}
+											</p>
+										</div>
+									</li>
+								</div>
 							</ul>
 						</div>
 					</div>
@@ -161,23 +188,6 @@ class TestDisplay extends React.Component{
 								:(<button type="button" className="btn btn-primary" onClick={this.deleteLike}> You like this Event </button>)
 						)} 
 					</p>
-				</div>
-				<div className="user-bio card-body">
-					<div className="user-container">
-						<div className="row bio-row">
-							<div className="profile-image-container">
-								<div className="profile-image">
-									<img src={`data:${this.state.users.mimeType};base64,${this.state.users.profilePicture}`} alt="" className="event-profile-pic"/>
-								</div>
-							</div>
-							<div className="event-user-container">
-								<div className="event-user">
-									<h4 className="event-username"> {this.state.users.name} </h4>
-									<p className="card-text event-bio"> {this.state.users.bio} </p>
-								</div>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		)
