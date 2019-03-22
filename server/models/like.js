f4316ed20c14a907357396e2628c4bf50582ff17
Josh Rubin <jkrubin42@gmail.com>
2019-03-21 22:00:33 -0700

    whoops

--- server/models/like.js
+++ server/models/like.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
     message: DataTypes.STRING,
-    liked: DataTypes.BOOLEAN
+    matched: DataTypes.BOOLEAN
   }, {});
   Like.associate = function(models) {
     Like.belongsTo(models.users, {foreignKey:'userId', as: 'user'})
